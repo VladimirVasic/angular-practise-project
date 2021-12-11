a406eacaaff2c864244642aa9c766bf7dabe1b6f
Vladimir Vasic <vvasic@its-group.com>
2021-12-11 18:18:25 +0100

    Displaying recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,30 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'test',
+      'http://www.picadostava.rs/wp-content/uploads/2017/02/Capricoza.jpg'
+    ),
+    new Recipe(
+      'A test recipe',
+      'test',
+      'http://www.picadostava.rs/wp-content/uploads/2017/02/Capricoza.jpg'
+    ),
+    new Recipe(
+      'A test recipe',
+      'test',
+      'http://www.picadostava.rs/wp-content/uploads/2017/02/Capricoza.jpg'
+    ),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
