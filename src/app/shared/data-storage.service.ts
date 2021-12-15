d84230209066196d9bdc45060835a5b34f5a6ece
Vladimir Vasic <vvasic@its-group.com>
2021-12-15 15:51:31 +0100

    Saving data to firebase

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,18 @@
+import { RecipeService } from './../recipes/recipe.service';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        'https://ng-course-reicpe-book-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        recipes
+      )
+      .subscribe(response => console.log(response));
+  }
+}
